e7d7dcd8b759c073d15d448bc1cfd65b7f4d1ca3
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2022-10-23 12:22:30 +0900

    initial commit on 2022.10.22

---
+++ README.md
@@ -0,0 +1,25 @@
+# # What's This?
+- インストールされた直後の Ubuntu 22.04 に，Python, PHP, Laravel, MySQL, Mecab などを自動的にインストールすることができます．
+
+# 設定方法
+
+## スクリプトの実行
+~~~
+./run.sh
+~~~
+
+## 設定を反映させる
+~~~
+exec $SHELL -l
+~~~
+
+
+## mysql のパスワード設定
+
+~~~
+vagrant@ubuntu2204:~$ sudo mysql
+mysql> ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'xxxx';
+mysql> FLUSH PRIVILEGES;
+mysql> exit
+vagrant@ubuntu2204:~$
+~~~
---
+++ run.sh
@@ -0,0 +1,39 @@
+test -z $(which ansible-playbook) && sudo apt install -y ansible
+sudo apt update
+sudo apt upgrade
+
+# 日本語化
+sudo locale-gen ja_JP.UTF-8
+export LANG=ja_JP.UTF-8
+export LANGUAGE=ja_JP:
+sudo update-locale LANG=ja_JP.UTF-8 LANGUAGE=ja_JP:
+
+# タイムゾーンの変更
+sudo timedatectl set-timezone Asia/Tokyo
+
+# man ページの日本語化
+sudo apt install manpages-ja manpages-ja-dev
+
+# apt での表示の日本語化
+sudo LANG=ja_JP.UTF-8 apt update
+
+# mecab-ipadic-neologd のビルドに必要
+sudo apt install build-essential
+
+# apache
+ansible-playbook 01-apache.yml --connection=local
+
+# MySQL
+ansible-playbook 02-mysql.yml --connection=local
+
+# PHP
+ansible-playbook 03-php.yml --connection=local
+
+# Laravel
+ansible-playbook 04-laravel.yml --connection=local
+
+# Mecab
+ansible-playbook 05-mecab.yml --connection=local
+
+# pyenv Anaconda
+ansible-playbook 06-pyenv-anaconda.yml --connection=local
