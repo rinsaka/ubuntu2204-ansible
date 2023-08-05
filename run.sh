8ec4055ce2d672ecb519605aa63e60d3e227a859
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2023-08-05 10:45:09 +0900

    prompt

--- run.sh
+++ run.sh
@@ -36,8 +36,9 @@ sudo apt install -y unzip
 sudo apt install -y curl
 
 # プロンプトを短く
-sed -i s/\\\\u@\\\\h\\\\\\[\\\\033\\[00m\\\\\\]:\\\\\\[\\\\033\\[01\;34m\\\\\\]\\\\w/\\\\u@:\\\\[\\\\033[01\;34m\\\\]\\\\W/ ~/.bashrc
-sed -i s/\\\\u@\\\\h:\\\\w\\\\/\\\\u@\\\\W\\\\/ ~/.bashrc
+sed -i s/\\\\u@\\\\h\\\\\\[\\\\033\\[00m\\\\\\]:\\\\\\[\\\\033\\[01\;34m\\\\\\]\\\\w/\\\\u@\\\\h:\\\\[\\\\033[01\;34m\\\\]\\\\W/ ~/.bashrc
+sed -i s/\\\\u@\\\\h:\\\\w\\\\/\\\\u@\\\\h:\\\\W\\\\/ ~/.bashrc
+
 
 # apache
 ansible-playbook 01-apache.yml --connection=local
